3dca225da194f27b959d0be0011eee547de882df
emmy16-glitch <74790322+emmy16-glitch@users.noreply.github.com>
2026-07-25 19:16:12 +0100

    Keep root dispatch capability-based and testable

--- vulnhunter/web/dashboard_dispatch_views.py
+++ vulnhunter/web/dashboard_dispatch_views.py
@@ -8,17 +8,21 @@ from django.views.decorators.cache import cache_control
 from django.views.decorators.http import require_GET
 
 from vulnhunter.web import conversational_views, views
-from vulnhunter.web.services import WebPermissionDenied, authorized_actor
+from vulnhunter.web.models import WebUserMapping
+from vulnhunter.web.services import role_policy
 
 
 @cache_control(private=True, no_store=True)
 @login_required
 @require_GET
 def dashboard_view(request: HttpRequest) -> HttpResponse:
-    """Use chat whenever the mapped account can create and read assessments."""
+    """Use chat whenever any mapped role can create an assessment."""
 
     try:
-        authorized_actor(request.user, required_actions=("scan.create", "scan.read"))
-    except WebPermissionDenied:
+        mapping = request.user.vulnhunter_mapping
+    except WebUserMapping.DoesNotExist:
         return views.dashboard_view(request)
-    return conversational_views.workspace_view(request)
+    roles = tuple(str(item) for item in mapping.product_roles if isinstance(item, str))
+    if role_policy().any_role_allows(roles, "scan.create"):
+        return conversational_views.workspace_view(request)
+    return views.dashboard_view(request)

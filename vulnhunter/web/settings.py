d573d74ccf4e7fba38bbd64421d9c14768653afe
emmy16-glitch <74790322+emmy16-glitch@users.noreply.github.com>
2026-07-27 13:18:36 +0100

    Install durable workspace middleware after authentication

--- vulnhunter/web/settings.py
+++ vulnhunter/web/settings.py
@@ -118,6 +118,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "vulnhunter.web.middleware.ConversationThreadMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "vulnhunter.web.middleware.ContentSecurityPolicyMiddleware",

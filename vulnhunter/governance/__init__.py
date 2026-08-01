5c404ca379e940cb99c8811f432c4d037c8f1a3a
EMMANUEL OKUNLOLA <74790322+emmy16-glitch@users.noreply.github.com>
2026-08-01 17:26:13 +0100

    Export campaign operations contracts

--- vulnhunter/governance/__init__.py
+++ vulnhunter/governance/__init__.py
@@ -1,5 +1,13 @@
 """Governed collection campaigns and authenticated review identities."""
 
+from vulnhunter.governance.campaign_operations import (
+    CampaignFamilyCoverage,
+    CampaignOperationsPrerequisite,
+    CampaignOperationsSnapshot,
+    CampaignReleasePackageStatus,
+    CampaignReviewWorkload,
+    assess_campaign_operations,
+)
 from vulnhunter.governance.models import (
     CampaignApplication,
     CampaignLimits,
@@ -26,12 +34,17 @@ from vulnhunter.governance.store import GovernanceStore
 __all__ = [
     "CampaignApplication",
     "CampaignApplicationProvenance",
+    "CampaignFamilyCoverage",
     "CampaignLimits",
+    "CampaignOperationsPrerequisite",
+    "CampaignOperationsSnapshot",
     "CampaignRecord",
     "CampaignReleasePackage",
     "CampaignReleasePackageError",
+    "CampaignReleasePackageStatus",
     "CampaignReleasePackageStore",
     "CampaignReviewProvenance",
+    "CampaignReviewWorkload",
     "CampaignScan",
     "DatasetReleaseManifest",
     "GovernanceStore",
@@ -39,6 +52,7 @@ __all__ = [
     "ReviewAssignment",
     "ReviewAttestation",
     "ReviewerIdentity",
+    "assess_campaign_operations",
     "build_campaign_release_package",
     "campaign_release_package_sha256",
     "create_campaign_release_package",
